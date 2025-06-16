cf785bd853ea5f56e3e6a5775fe8767547dced0a
Rachit Srivastava <rachit298@gmail.com>
2025-06-16 21:53:56 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
 const express = require("express");
-
+const { connectDB } = require('./config/database');
 const app = express();
+const User = require("./models/user");
 
-app.use("/test", (req, res) => {
-    res.send("This is the test path '/test'");
+// first connect to DB. If connected successfully, then only listen to port
+connectDB().then(() => {
+    console.log("Database connection established.");
+    app.listen(1234, () => {
+        console.log("Server is listening on port 1234.");
+    });
+}).catch((err) => {
+    console.log("Database cannot be connected.");
 })
 
-app.use("/hello", (req, res) => {
-    res.send("This is the hello path '/hello'");
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName: "Rachit",
+        lastName: "S.",
+        emailId: "rachit@sri.com",
+        password: "rachit123"
+    }
+    // Creating a new instance of the User model
+    const user = new User(userObj);
+    try {
+        await user.save();
+        res.send("User created successfully.");
+    }
+    catch (err) {
+        res.status(400).send("Error while creating user. Error message => " + err.message);
+    }
 })
-
-app.use("/", (req, res) => {
-    res.send("This is the base path '/'");
-})
-
-app.listen(1234, () => {
-    console.log("Server is listening at port 1234.");
-});
