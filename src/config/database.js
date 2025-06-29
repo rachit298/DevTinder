0078aa1adbd16f55f6eb07a40c4791edce719a16
Rachit Srivastava <69860596+rachit298@users.noreply.github.com>
2025-06-29 20:36:32 +0530

    Changed connection string for security reason.

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     //connected to cluster(collection of Databases)
-    await mongoose.connect("mongodb+srv://rachit298:CPwSRVxl6cfkaDDC@namastenodejs.6meuyxz.mongodb.net/devTinder");
+    await mongoose.connect("mongodb+srv://username:password@clustername.6meuyxz.mongodb.net/devTinder");
 }
 
 module.exports = {
